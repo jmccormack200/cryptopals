9d5fa470b2a88a6c8cb6ffdf6b7d4fda92e48578
John McCormack <mccormack.wgsi@gmail.com>
2016-07-10 14:17:37 -0400

    Fixed file structure

--- Challenge3/xorCipher.py
+++ Challenge3/xorCipher.py
@@ -41,3 +41,5 @@ def body(data):
 if __name__ == "__main__":
 	string = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
 	body(string)
+	string = "ETAOIN SHRDLU"
+	body(string)
